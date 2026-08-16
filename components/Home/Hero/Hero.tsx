bad640db59edf53b5ea53e815e291df54ba0b4db
rishavkaushik70 <rishavkaushik70@gmail.com>
2026-08-17 02:52:07 +0530

    Fix project image paths

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
       <ParticlesHero />
       <div className="relative z-10 flex flex-col items-center">
         <Image
-          src={"/Images/Myprofile.png"}
+          src={"/Images/myprofile.png"}
           alt="heroimage"
           width={150}
           height={150}
